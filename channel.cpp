6958a8ce3749d87a4f9ade3e7d2cb7e9deba7b5a
wen2ms <1819509111@qq.com>
2025-05-13 00:20:46 +0800

    Refactor: Channel

---
+++ channel.cpp
@@ -0,0 +1,21 @@
+#include "channel.h"
+
+Channel::Channel(int fd, int events, handle_func read_func, handle_func write_func, handle_func destroy_func, void* arg)
+    : fd_(fd),
+      events_(events),
+      read_callback_(read_func),
+      write_callback_(write_func),
+      destroy_callback_(destroy_func),
+      arg_(arg) {}
+
+void Channel::write_event_enable(bool flag) {
+    if (flag) {
+        events_ |= static_cast<int>(FDEvent::kWriteEvent);
+    } else {
+        events_ &= ~static_cast<int>(FDEvent::kReadEvent);
+    }
+}
+
+bool Channel::is_write_event_enable() {
+    return events_ & static_cast<int>(FDEvent::kWriteEvent);
+}
---
+++ channel.h
@@ -0,0 +1,38 @@
+#pragma once
+
+using handle_func = int (*)(void*);
+
+enum class FDEvent {
+    kTimeout = 0x01,
+    kReadEvent = 0x02,
+    kWriteEvent = 0x04
+};
+
+class Channel {
+  public:
+    Channel(int fd, int events, handle_func read_func, handle_func write_func, handle_func destroy_func, void* arg);
+
+    void write_event_enable(bool flag);
+    bool is_write_event_enable();
+
+    inline int get_event() {
+        return events_;
+    }
+
+    inline int get_socket() {
+        return fd_;
+    }
+
+    inline const void* get_arg() {
+        return arg_;
+    }
+
+    handle_func read_callback_;
+    handle_func write_callback_;
+    handle_func destroy_callback_;
+
+  private:
+    int fd_;
+    int events_;
+    void* arg_;
+};
