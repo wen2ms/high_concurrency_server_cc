54b2ba5dd18ac47ab61b02e081a1021314c78d19
wen2ms <1819509111@qq.com>
2025-05-16 21:49:42 +0800

    Refactor: HttpResponse

---
+++ http_response.cpp
@@ -0,0 +1,41 @@
+#include "http_response.h"
+
+#include <stdio.h>
+
+#include "log.h"
+#include "tcp_connection.h"
+
+HttpResponse::HttpResponse() {
+    status_code_ = StatusCode::kUnknown;
+    headers_.clear();
+    file_name_ = std::string();
+    send_data_func_ = nullptr;
+}
+
+HttpResponse::~HttpResponse() {}
+
+void HttpResponse::add_header(const std::string& key, const std::string& value) {
+    if (key.empty() || value.empty()) {
+        return;
+    }
+
+    headers_.insert(std::make_pair(key, value));
+}
+
+void HttpResponse::prepare_msg(Buffer* send_buf, int socket) {
+    char tmp[1024] = {0};
+    int code = static_cast<int>(status_code_);
+    sprintf(tmp, "HTTP/1.1 %d %s\r\n", code, info_.at(code).data());
+    send_buf->append_string(tmp);
+
+    for (auto it = headers_.begin(); it != headers_.end(); it++) {
+        sprintf(tmp, "%s: %s\r\n", it->first.data(), it->second.data());
+        send_buf->append_string(tmp);
+    }
+    send_buf->append_string("\r\n");
+#ifndef MSG_SEND_AUTO
+    send_buf->send_data(socket);
+#endif
+
+    send_data_func_(file_name_, send_buf, socket);
+}
---
+++ http_response.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <functional>
+#include <map>
+
+#include "buffer.h"
+
+enum class StatusCode {
+    kUnknown,
+    kOK = 200,
+    kMovedPermanently = 301,
+    kMovedTemporarily = 302,
+    kBadRequest = 400,
+    kNotFound = 404
+};
+
+class HttpResponse {
+  public:
+    HttpResponse();
+    ~HttpResponse();
+
+    void add_header(const std::string& key, const std::string& value);
+    void prepare_msg(Buffer* send_buf, int socket);
+
+    inline void set_status_code(StatusCode code) {
+        status_code_ = code;
+    }
+
+    inline void set_file_name(const std::string& name) {
+        file_name_ = name;
+    }
+
+    std::function<void(const std::string&, Buffer*, int)> send_data_func_;
+
+  private:
+    StatusCode status_code_;
+    std::string status_msg_;
+    std::string file_name_;
+    std::map<std::string, std::string> headers_;
+    const std::map<int, std::string> info_ = {
+        {200, "OK"}, {301, "Moved Permanently"}, {302, "Moved Temporarily"}, {400, "Bad Request"}, {404, "Not Found"}};
+};
